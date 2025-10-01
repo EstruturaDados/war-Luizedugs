a1808316789f52c64ebaafdb7c8f370e9a48ec22
Luizedugs <luizeduardomastetek@gmail.com>
2025-10-01 00:27:54 +0000

    - Implemento função para cadastro de territórios com alocação dinâmica de memória  - Realizo também a criação de simulação dos ataques de territórios criando a função void atacar  ultilizando também ponteiros não apenas nessa função, porém nas demais para manipulação direta  das estruturas e passagem eficiente entre funções. - incluo 'free()' para o controle de memória com liberação. - Ultilizo srand(time(NULL)) para garantir aleatoriedade de ataques dos territórios. - Modifico um pouco a estrutura do código, para deixar mais fácil para manutenção caso seja necesário.

--- projetowar.c
+++ projetowar.c
@@ -1,71 +1,146 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define TAM_NOME 50
-#define TAM_COR 20
-#define TAM_Territorio 5
-
-// Definição da struct
-//Ela armazena:
-// - nome do territórioo, a cor do exército que ocupa o território e armazena a quantidades de tropas
-
-
-struct Territorio { 
-
-    char nome [30];
-    char cor [10];
+typedef struct {
+    char nome[30];
+    char cor[10];
     int tropas;
-};
+} Territorio;
 
-int main () {
-struct Territorio territorios[TAM_Territorio]; // Vetor de amarzenação dos 5 terriotiross
-    int menu_principal = 1;
-    while (menu_principal !=0) // Laço while permite que o menu seja exibido continuamente
-    {   printf("==============================\n");
-        printf("    MENU PRINCIPAL JOGO WAR   \n");
-        printf("==============================\n");
-        printf("1 - CADASTRAR TERRITORIOS     \n");
-        printf("2 - VISUALIZAR CADASTRO       \n");
-        printf("Escolha uma opcao: ");
-        int opcao;
-        scanf("%d", &opcao);
-
-
-    switch (opcao) //Estrutura switch, serve para tratar as opções do menu do jogo
-    {
-    case 1: 
-          printf("\n======= CADASTRO DE TERRITORIOS =======\n");
-          for (int i = 0; i < TAM_Territorio; i++) {
-                   
-          printf("Cadastro dos territorios: \n");
-          printf("Territorio %d\n", i + 1);
-          printf("Nome :" );
-          scanf("%s", territorios[i].nome); //Leitura do nome do territorio
-          printf("Informe a cor do exercito: \n");
-          scanf("%s", territorios[i].cor); //Leitura da cor do territorio
-          printf("informe a qtnd de tropas: \n");
-          scanf("%d", &territorios[i].tropas); //Leitura da quantidades de territorios que irá ser cadastrado
-            }
-        break;
-    
-    case 2:
-         // Exibição dos dados que foram cadastrados
-        printf("\n===CADASTRO DO JOGADOR===\n");
-        for (int i = 0; i < TAM_Territorio; i++) {
-        printf("Territorio %d:\n\n", i + 1);
-        printf("============================\n");
-        printf("Nome: %s\n", territorios[i].nome);
-        printf("============================\n");
-        printf("Cor: %s\n", territorios[i].cor);
-        printf("============================\n");
-        printf("Tropas: %d\n", territorios[i].tropas);
-        printf("============================\n");
-        }
-            
-        break;
-      
-      }
-     }
-    return 0;
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
+//Função de cadastramento de territorios
+void cadastrarTerritorios(Territorio* territorios, int qtd) {
+    printf("\n======= CADASTRO DE TERRITORIOS =======\n");
+    for (int i = 0; i < qtd; i++) {
+        printf("Cadastro do Territorio %d:\n", i + 1);
+        printf("Nome: ");
+        scanf("%s", territorios[i].nome);
+        printf("Cor do exercito: ");
+        scanf("%s", territorios[i].cor);
+        printf("Quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+        limparBuffer();
+    }
+}
+//Função de exibir o cadastro de territorios
+void exibirTerritorios(Territorio* territorios, int qtd) {
+    printf("\n======= EXIBIÇÃO DOS TERRITORIOS =======\n");
+    for (int i = 0; i < qtd; i++) {
+        printf("Territorio %d:\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor: %s\n", territorios[i].cor);
+        printf("Tropas: %d\n", territorios[i].tropas);
+        printf("-----------------------------\n");
+    }
+}
+//Função de atacar os territorios
+void atacar(Territorio* atacante, Territorio* defensor) {
+    printf("\nSimulando ataque de '%s' para '%s'...\n", atacante->nome, defensor->nome);
+
+    if (atacante->tropas < 2) {
+        printf("O atacante precisa de pelo menos 2 tropas para atacar!\n");
+        return;
+    }
+
+    int dadoAtacante = (rand() % 6) + 1;
+    int dadoDefensor = (rand() % 6) + 1;
+
+    printf("Dado do atacante: %d\n", dadoAtacante);
+    printf("Dado do defensor: %d\n", dadoDefensor);
+
+    if (dadoAtacante > dadoDefensor) {
+        printf("O atacante venceu!\n");
+
+        strcpy(defensor->cor, atacante->cor);
+        defensor->tropas = atacante->tropas / 2;
+        atacante->tropas -= defensor->tropas;
+
+    } else {
+        printf("O defensor defendeu seu territorio com sucesso\n");
+        atacante->tropas -= 1;
+    }
+}
+
+void liberarMemoria(Territorio* territorios) {
+    free(territorios); //Libera a memoria alocada
+    printf("A memória liberada com sucesso!\n");
+}
+
+int main() {
+    int opcao; //Menu de opções para usuario escolher
+    int qtdTerritorios = 0;
+    Territorio* territorios = NULL;
     
+    do {
+        printf("\n======= MENU PRINCIPAL =======\n");
+        printf("1 - Cadastrar Territorios\n");
+        printf("2 - Exibir Territorios\n");
+        printf("3 - Atacar\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        limparBuffer();
+
+        switch (opcao) {
+            case 1:
+                 srand(time(NULL)); // Inicializa aleatoriedade
+                 int qtdTerritorios;
+                 printf("Quantos territorios deseja cadastrar? ");
+                 scanf("%d", &qtdTerritorios);
+                 limparBuffer();
+                 // Aloca memória para o número de territórios informados
+                Territorio* territorios = (Territorio*) malloc(qtdTerritorios * sizeof(Territorio));
+                 if (territorios == NULL) {
+                    printf("Erro ao alocar memória.\n");
+                    return 1;
+                 }
+                
+                cadastrarTerritorios(territorios, qtdTerritorios);
+                break;
+            case 2:
+                 // Verifica se há territórios cadastrados
+                exibirTerritorios(territorios, qtdTerritorios);
+                break;
+            // Verifica se há pelo menos dois territórios para realizar um ataque
+            case 3: {
+                if (territorios == NULL || qtdTerritorios < 2) {
+                printf("Cadastre pelo menos 2 territórios antes de atacar.\n");
+                break;
+                }
+                int idAtacante, idDefensor;
+                exibirTerritorios(territorios, qtdTerritorios);
+
+                printf("Escolha o numero do territorio ATACANTE (1 a %d): ", qtdTerritorios);
+                scanf("%d", &idAtacante);
+                printf("Escolha o numero do territorio DEFENSOR (1 a %d): ", qtdTerritorios);
+                scanf("%d", &idDefensor);
+                limparBuffer();
+
+                if (idAtacante == idDefensor || 
+                    idAtacante < 1 || idAtacante > qtdTerritorios || 
+                    idDefensor < 1 || idDefensor > qtdTerritorios) {
+                    printf("IDs inválidos para ataque.\n");
+                } else {
+                    atacar(&territorios[idAtacante - 1], &territorios[idDefensor - 1]);
+                }
+                break;
+            }
+            case 0:
+                liberarMemoria(territorios);
+                printf("Saindo do jogo...\n");
+                break;
+            default:
+                printf("Opcao invalida.\n");
+        }
+
+    } while (opcao != 0); // Repete o menu até o usuário escolher sair escolhendo a opção "0"
+        if (territorios != NULL) {
+        liberarMemoria(territorios);
+         }
+    return 0; //Finliza o progama
+}
